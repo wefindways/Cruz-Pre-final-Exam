99c13e3533e1621a66e9b41b5db048d8544a1a05
= <marcjosephcruz24@gmail.com>
2025-11-04 10:36:11 +0800

    feat: enhance header component and include Add Product button

--- src/components/Header/CartIcon.jsx
+++ src/components/Header/CartIcon.jsx
@@ -2,14 +2,19 @@ import { Handbag } from "lucide-react";
 
 const CartIcon = () => {
   return (
-    <div className="relative text-white">
-      <Handbag
-        size={26}
-        className="cursor-pointer text-gray-700 hover:text-violet-600 transition-colors"
-      />
-      <span className="absolute -top-2 -right-2 flex justify-center items-center bg-red-600 rounded-full w-5 h-5 text-xs font-semibold">
-        2
-      </span>
+    <div className="flex gap-3 items-center text-white hover:bg-gray-100 py-3 px-5 active:bg-gray-200 transition-colors rounded-full cursor-pointer">
+      <div className="relative">
+        <Handbag
+          size={26}
+          className="cursor-pointer text-gray-700"
+        />
+        <span className="absolute -top-2 -right-2 flex justify-center items-center bg-red-600 rounded-full w-5 h-5 text-xs font-semibold">
+          2
+        </span>
+      </div>
+      <div>
+        <p className="text-gray-800 font-semibold">Cart</p>
+      </div>
     </div>
   );
 };
--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -2,7 +2,7 @@ import products from "../hooks/products";
 
 const ProductDisplay = () => {
   return (
-    <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-15 gap-8">
+    <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 pb-15 gap-8">
       {products.map((product, index) => (
         <div
           key={index}
