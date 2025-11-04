fea4e4f6b1991be69ff26f5be05f2024fb051cc8
Mac <marcjosephcruz24@gmail.com>
2025-11-05 00:36:09 +0800

    feat: functional modal for adding products (#11)
    * feat: add modal component for Add Product button
    
    * feat: require all inputs
    
    * feat: add functional modal with static content from user input

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,8 +1,6 @@
-import products from "../hooks/products";
-
-const ProductDisplay = () => {
+const ProductDisplay = ({ products }) => {
   return (
-    <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 pb-15 gap-8">
+    <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pb-15 gap-8">
       {products.map((product, index) => (
         <div
           key={index}
