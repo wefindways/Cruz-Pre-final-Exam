ab1ca1b4837bcf743f4ec75de32c5d57c42334b7
= <marcjosephcruz24@gmail.com>
2025-11-05 21:47:18 +0800

    refactor: restructure app with reusable Layout and dedicated Home page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,33 +1,12 @@
 import Header from "../components/Header/Header";
-import ProductDisplay from "../components/ProductDisplay";
 import Footer from "../components/Footer";
-import Category from "../components/Category";
-import { useState } from "react";
-import productsData from "../hooks/products";
 
-const Layout = () => {
-  const [products, setProducts] = useState(productsData);
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  const handleAddProduct = (newProduct) => {
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
-  };
+const Layout = ({ children, onAddProduct }) => {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <Header onAddProduct={handleAddProduct} />
-
-      <main className="grow">
-        <Category
-          products={products}
-          setSelectedCategory={setSelectedCategory}
-        />
-        <ProductDisplay
-          products={products}
-          selectedCategory={selectedCategory}
-        />
-      </main>
-
+      <Header onAddProduct={onAddProduct} />
+      <main className="grow">{children}</main>
       <Footer />
     </div>
   );
