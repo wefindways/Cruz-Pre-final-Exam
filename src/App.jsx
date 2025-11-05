27444bde520fd84523dbeef184f50ff409184829
Mac <marcjosephcruz24@gmail.com>
2025-11-05 21:59:35 +0800

    refactor: restructure app with reusable Layout and dedicated Home page (#14)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Layout from "./layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
