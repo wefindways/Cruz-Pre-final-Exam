d435c31d1dac0926607ccb58ca848e81a6fcc129
= <marcjosephcruz24@gmail.com>
2025-11-07 00:57:11 +0800

    refactor: update Details page to receive product data via props

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,13 +1,22 @@
+// Details.jsx
+import { useParams } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import ProductDetails from "../components/ProductDetails";
 import Layout from "../layout/Layout";
 
-const Details = () => {
+const Details = ({ products }) => {
+  const { id } = useParams();
+  const product = products.find((p) => p.id === Number(id));
+
   return (
     <Layout>
       <div className="flex flex-col gap-5 max-w-5xl mx-auto mt-10 p-8">
         <BackButton />
-        <ProductDetails />
+        {product ? (
+          <ProductDetails product={product} />
+        ) : (
+          <p className="text-center text-gray-500">Product not found</p>
+        )}
       </div>
     </Layout>
   );
