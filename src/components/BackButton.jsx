3bce0e7386c891582b3f0e5a3134e57720852960
= <marcjosephcruz24@gmail.com>
2025-11-06 15:57:52 +0800

    style: adjust BackButton padding

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -5,7 +5,7 @@ const BackButton = () => {
   return (
     <Link to="/">
       <div className="flex">
-        <div className="flex gap-3 py-2 pl-5 pr-12 rounded-full hover:bg-gray-200 transition-colors duration-200">
+        <div className="flex gap-3 py-3 pl-5 pr-12 rounded-full hover:bg-gray-200 transition-colors duration-200">
           <ArrowLeft />
           <span className="font-semibold">Back to home</span>
         </div>
