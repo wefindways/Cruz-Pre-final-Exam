26a3d0dfc5ea9c31f65b80a1ef779df7aead4634
= <marcjosephcruz24@gmail.com>
2025-11-07 18:29:18 +0800

    refactor: move Add Product modal logic to Home component for better state management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,30 @@
 import Layout from "../layout/Layout";
 import ProductDisplay from "../components/ProductDisplay";
 import Category from "../components/Category";
+import AddProduct from "../components/Header/AddProduct";
+import Modal from "../components/Modal";
 import { useState } from "react";
 
 const Home = ({ products, onAddProduct }) => {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <Layout onAddProduct={onAddProduct}>
       <Category products={products} setSelectedCategory={setSelectedCategory} />
-      <ProductDisplay products={products} selectedCategory={selectedCategory} />
+      <div className="flex flex-col gap-5 max-w-7xl mx-auto">
+        <AddProduct onClick={() => setIsModalOpen(true)} />
+        <ProductDisplay
+          products={products}
+          selectedCategory={selectedCategory}
+        />
+      </div>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
+          <Modal setIsModalOpen={setIsModalOpen} onAddProduct={onAddProduct} />
+        </div>
+      )}
     </Layout>
   );
 };
